f61fe2298f043fcb0fcc962008c4f8c5ed1d4697
leonwatson2 <leon@vlw2.com>
2024-08-17 22:33:51 -0500

    Added middle of screen popup for lsp popups using telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,21 @@ return {
     end
   },
   {
-    "nvim-telescope/telescope-ui-select.nvim"
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { 'node_modules' }
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
   }
 }
 
