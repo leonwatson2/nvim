5148ba2440c0ebd99f73e0960883ecfdb4477338
leonwatson2 <leon@vlw2.com>
2024-08-17 22:23:40 -0500

    Added lsp for javascript/typescript and keymaps to open definitions and code action

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "tsserver" }
+      
+        
       })
     end
   },
@@ -19,8 +21,10 @@ return {
       local lspconfig = require("lspconfig")
 
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
