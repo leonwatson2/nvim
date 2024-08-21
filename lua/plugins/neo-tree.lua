7a021b6a9b9f760f4913f626ab9560669f57c741
Leon Watson <leon@vlw2.com>
2024-08-21 15:48:35 -0500

    extra

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,26 +1,31 @@
-return 
-{
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true, -- hidden files can be toggled with shift+h
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          never_show = {},
-        },
-      }
-    },
-    config = function()
-      vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
-    end
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		window = {
+			mappings = {
 
+				["none"] = "open_with_window_picker",
+				["z"] = "expand_all_nodes",
+				["l"] = "toggle_node",
+			},
+		},
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				show_hidden_count = true, -- hidden files can be toggled with shift+h
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				never_show = {},
+			},
+		},
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+	end,
 }
-
