498632f70ea0efd04e047516c4d70167dc1a4f6c
leonwatson2 <leon@vlw2.com>
2024-08-22 03:09:36 -0500

    Neo-tree config works wow

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,31 +1,32 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	opts = {
-		window = {
-			mappings = {
-
-				["none"] = "open_with_window_picker",
-				["z"] = "expand_all_nodes",
-				["l"] = "toggle_node",
-			},
-		},
-		filesystem = {
-			filtered_items = {
-				visible = true,
-				show_hidden_count = true, -- hidden files can be toggled with shift+h
-				hide_dotfiles = false,
-				hide_gitignored = false,
-				never_show = {},
-			},
-		},
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        mappings = {
+          ["w"] = "none",
+          ["none"] = "open_with_window_picker",
+          ["z"] = "expand_all_nodes",
+          ["l"] = "toggle_node",
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true, -- hidden files can be toggled with shift+h
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          never_show = {},
+        },
+      },
+    }
+    )
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+  end,
 }
