638352e66caa2f48113b9c2dabab5e8892a33e27
leonwatson2 <leon@vlw2.com>
2024-08-22 03:16:22 -0500

    Updated custom commands to work better

--- lua/custom-commands.lua
+++ lua/custom-commands.lua
@@ -5,15 +5,12 @@ vim.api.nvim_create_user_command("Config", function ()
   if(osName == "Windows_NT") then
     vim.cmd("cd ~/AppData/Local/nvim")
   elseif(osName == "Darwin") then
-    vim.cmd("~/.config/nvim")
+    vim.cmd("cd ~/.config/nvim/")
   end
-  vim.cmd(":Neotree filesystem reveal left<CR>")
+  vim.cmd(":Neotree reveal <CR>")
 
 end, {})
  vim.api.nvim_create_user_command("Fun", function ()
-  local osName = vim.loop.os_uname().sysname
-  if(osName == "Windows_NT") then
-    vim.cmd("cd ~/Coding")
-  end
-  vim.cmd(":Neotree filesystem reveal left<CR>")
+  vim.cmd("cd ~/Coding")
+  vim.cmd(":Neotree reveal <CR>")
 end, {})
