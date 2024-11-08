e1ef4ef5eba36e4f9e691061dd2cf3bcd84152cc
Leon Watson <leon@vlw2.com>
2024-11-08 14:33:01 -0600

    add vim surround

--- lua/plugins/vim-surround.lua
+++ lua/plugins/vim-surround.lua
@@ -1,3 +1,9 @@
 return {
-  "tpope/vim-surround"
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+        })
+    end
 }
