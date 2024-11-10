47139a50562892882c9051857cfa84310c283baf
leonwatson2 <leon@vlw2.com>
2024-11-10 11:26:14 -0600

    Updated lsp config to use ts_ls for non mac computers and ts_ls. Runninf into inconsistent naming between the two through mason install.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,8 +8,10 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
+      local isMac = vim.loop.os_uname().sysname == "Darwin"
+      local tsserver = isMac and "tsserver" or "ts_ls"
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "ts_ls" },
+				ensure_installed = { "lua_ls", "gopls", tsserver },
 			})
 		end,
 	},
@@ -22,8 +24,9 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-
-			lspconfig.ts_ls.setup({
+      local isMac = vim.loop.os_uname().sysname == "Darwin"
+      local tsserver = isMac and lspconfig.tsserver or lspconfig.ts_ls
+			tsserver.setup({
 				init_options = {
 					preferences = {
 						importModuleSpecifierPreference = "non-relative",
